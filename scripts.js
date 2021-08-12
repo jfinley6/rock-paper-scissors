686a1f5f11f4c1c468208939cfca4443409d665f
John Finley <jfinley6@mail.csuchico.edu>
2021-08-11 21:08:43 -0600

    Added a simple UI with buttons.

--- scripts.js
+++ scripts.js
@@ -1,59 +1,31 @@
-let moves = ["rock", "paper", "scissors"];
-
-function game() {
-    alert("Welcome to rock, paper, scissors. This is a best of 5 so whoever gets to 3 wins first is the victor!");
-    singleRound();
-}
+let moves = ['Rock', 'Paper', 'Scissors'];
+let weapons = {};
 
 function computerPlay() {
     randomMove = moves[Math.floor(Math.random()*moves.length)];
 }
 
-function singleRound() {
-    let playerMove = String(prompt("Please select rock, paper, or scissors."));
-    let lowerPlayerMove = playerMove.toLowerCase();
-    computerPlay();
-    if (playerMove == "rock" && randomMove == "scissors") {
-        alert("The computer chose scissors. You won!");
-        playerWins = playerWins + 1;
-        alert("The score is now " + playerWins + "-" + computerWins);
-    }   else if (lowerPlayerMove == "rock" && randomMove == "paper") {
-        alert("The computer chose paper. The computer won...");
-        computerWins = computerWins + 1;
-        alert("The score is now " + playerWins + "-" + computerWins);
-    }   else if (lowerPlayerMove == "scissors" && randomMove == "paper") {
-        alert("The computer chose paper. You won!");
-        playerWins = playerWins + 1;
-        alert("The score is now " + playerWins + "-" + computerWins);
-    }   else if (lowerPlayerMove == "scissors" && randomMove == "rock") {
-        alert("The computer chose rock. The computer won...");
-        computerWins = computerWins + 1;
-        alert("The score is now " + playerWins + "-" + computerWins);
-    }   else if  (lowerPlayerMove == "paper" && randomMove == "rock") {
-        alert("The computer chose rock. You won!");
-        playerWins = playerWins + 1;
-        alert("The score is now " + playerWins + "-" + computerWins);
-    }   else if (lowerPlayerMove == "paper" && randomMove == "scissors") {
-        alert("The computer chose scissors. The computer won.");
-        computerWins = computerWins + 1;
-        alert("The score is now " + playerWins + "-" + computerWins);
-    }   else if (lowerPlayerMove == randomMove) {
-        alert("It's a tie! Please please again.");
-        singleRound();
-    }   else {
-        alert("You need to enter rock, paper, or scissors!");
-    }  
-    if (playerWins == 3) {
-        alert("The game is over. You are the victor!");
-        playerWins = 0;
-        computerWins =0;
-        alert("Thanks for playing!");
-    }   else if (computerWins == 3) {
-        alert("The game is over. The computer is the victor...")
-        playerWins = 0;
-        computerWins =0;
-        alert("Thanks for playing!");
-    }   else {
-        singleRound();
-    }
-}
+const buttons = document.querySelectorAll('button');
+const result = document.querySelector('.game-result')
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        computerPlay();
+        myMove = button.innerHTML;
+        weapons = {
+            Rock: {weakTo: 'Paper', strongTo: 'Scissors'},
+            Paper: {weakTo: 'Scissors', strongTo: 'Rock'},
+            Scissors: {weakTo: 'Rock', strongTo: 'Paper'}
+         }
+         if (weapons[myMove].strongTo === randomMove) {
+            message = "You won!";
+         }
+         else if (weapons[myMove].weakTo === randomMove) {
+            message = "You lost!";
+         }
+         else {
+            message = "It's a tie.";
+         }
+        result.innerHTML = `You chose ${myMove}. The computer chose ${randomMove}. ${message}`;
+    });
+});
