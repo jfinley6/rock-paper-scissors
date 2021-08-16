580fc74d3d76255bfcb6f6a7928eff1bd87a3fd6
John Finley <jfinley6@mail.csuchico.edu>
2021-08-15 21:46:23 -0400

    Finished my game

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 let moves = ['Rock', 'Paper', 'Scissors'];
+let playerScore = 0;
+let computerScore = 0;
+
 
 function computerPlay() {
     randomMove = moves[Math.floor(Math.random()*moves.length)];
@@ -6,6 +9,8 @@ function computerPlay() {
 
 const buttons = document.querySelectorAll('button');
 const result = document.querySelector('.game-result')
+const gameResult = document.querySelector('.score')
+const reset = document.getElementsByClassName('reset-button')[0];
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -20,11 +25,54 @@ buttons.forEach((button) => {
             message = "You won!";
          }
          else if (weapons[myMove].weakTo === randomMove) {
-            message = "You lost!";
+            message = "You lost...";
          }
          else {
             message = "It's a tie.";
          }
         result.innerHTML = `You chose ${myMove}. The computer chose ${randomMove}. ${message}`;
+        gameScore();
     });
+});
+
+function gameScore() {
+    if (message === "You won!") {
+        playerScore += 1;
+        gameResult.innerHTML = `The game is now: ${playerScore} - ${computerScore}`;
+    }
+    else if (message === "You lost...") {
+        computerScore += 1;
+        gameResult.innerHTML = `The game is now: ${playerScore} - ${computerScore}`;
+    }
+    gameEnd()
+};
+
+function gameEnd() {
+    if (playerScore == "5") {
+        gameResult.innerHTML = `The score is ${playerScore} - ${computerScore}, you won!`
+        disabled(true);
+        document.getElementsByClassName('reset-button')[0].style.visibility = 'visible';
+    }
+    else if (computerScore == "5") {
+        gameResult.innerHTML = `The score is ${playerScore} - ${computerScore}, you lost...`
+        disabled(true);
+        document.getElementsByClassName('reset-button')[0].style.visibility = 'visible';
+    }
+};
+
+function disabled(statement) {
+    document.getElementById('rock').disabled = statement;
+    document.getElementById('paper').disabled = statement;
+    document.getElementById('scissors').disabled = statement;
+}
+
+
+
+reset.addEventListener('click', () => {
+    playerScore = 0;
+    computerScore = 0;
+    disabled(false);
+    gameResult.innerHTML = '';
+    reset.style.visibility = 'hidden';
+    result.innerHTML = '';
 });
